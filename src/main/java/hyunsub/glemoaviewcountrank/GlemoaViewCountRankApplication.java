cec8e8fe6a4eb488db62021e0934d31e1cc53b9e
신현섭 <didakd22@naver.com>
2025-10-17 17:07:52 +0900

    first commit

---
+++ src/main/java/hyunsub/glemoaviewcountrank/GlemoaViewCountRankApplication.java
@@ -0,0 +1,13 @@
+package hyunsub.glemoaviewcountrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlemoaViewCountRankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlemoaViewCountRankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyunsub/glemoaviewcountrank/GlemoaViewCountRankApplicationTests.java
@@ -0,0 +1,13 @@
+package hyunsub.glemoaviewcountrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlemoaViewCountRankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
